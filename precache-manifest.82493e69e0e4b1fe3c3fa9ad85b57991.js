d22ea9317243dbc8fc1b516affed062e0b50e6af
wbylica <w.bylica@il-pib.pl>
2023-01-25 09:13:26 +0100

    Updates

---
+++ precache-manifest.82493e69e0e4b1fe3c3fa9ad85b57991.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddf32840255fcdb1eeeb",
+    "url": "/saturdaylive-new-page/static/js/main.ddf32840.chunk.js"
+  },
+  {
+    "revision": "1976aacbfb0f4548e6bf",
+    "url": "/saturdaylive-new-page/static/css/1.3f4239cf.chunk.css"
+  },
+  {
+    "revision": "1976aacbfb0f4548e6bf",
+    "url": "/saturdaylive-new-page/static/js/1.1976aacb.chunk.js"
+  },
+  {
+    "revision": "ad937b438cc5416aa65f",
+    "url": "/saturdaylive-new-page/static/js/2.ad937b43.chunk.js"
+  },
+  {
+    "revision": "cfe131b7a7947b35d0b2",
+    "url": "/saturdaylive-new-page/static/js/3.cfe131b7.chunk.js"
+  },
+  {
+    "revision": "568ff50645525a386e3d",
+    "url": "/saturdaylive-new-page/static/js/4.568ff506.chunk.js"
+  },
+  {
+    "revision": "c6c7273af3703c09aa13",
+    "url": "/saturdaylive-new-page/static/js/5.c6c7273a.chunk.js"
+  },
+  {
+    "revision": "43c94386842d96ede422",
+    "url": "/saturdaylive-new-page/static/js/runtime~main.43c94386.js"
+  },
+  {
+    "revision": "09511483bd8390e42736ee78411339f4",
+    "url": "/saturdaylive-new-page/static/media/facebook.09511483.svg"
+  },
+  {
+    "revision": "a2581432a9739f754c15e8225630f4e4",
+    "url": "/saturdaylive-new-page/static/media/instagram.a2581432.svg"
+  },
+  {
+    "revision": "58f7a1b6daf239b53beed66113cd0f67",
+    "url": "/saturdaylive-new-page/static/media/youtube.58f7a1b6.svg"
+  },
+  {
+    "revision": "b2ac049aaeb344d3ce93c7aabdff64a5",
+    "url": "/saturdaylive-new-page/static/media/soundcloud.b2ac049a.svg"
+  },
+  {
+    "revision": "aef324e09f7a6a715fc96c8bb8327fd4",
+    "url": "/saturdaylive-new-page/static/media/menu-down.aef324e0.svg"
+  },
+  {
+    "revision": "20b1f737e80f686d931dad703866ce40",
+    "url": "/saturdaylive-new-page/static/media/mailbox.20b1f737.svg"
+  },
+  {
+    "revision": "465d82b10daf21e29e4802228ee9f1a5",
+    "url": "/saturdaylive-new-page/index.html"
+  }
+];
