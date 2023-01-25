e7c4493b810fd20a5f2c29dce3cafa14d7104884
Wojciech Bylica <wojtekbyl@gmail.com>
2023-01-25 02:13:59 +0100

    no lazy loading of map

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -19,8 +19,8 @@ export const Contact = () => {
         <StyledMapIframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2443.7556541686185!2d21.035567551120305!3d52.22965696535906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecd894bc9d9b5%3A0x2e8fdfac02717940!2ssaturdaylive%20-%20zesp%C3%B3%C5%82%20muzyczny%20na%20imprez%C4%99%20firmow%C4%85%20i%20wesele!5e0!3m2!1spl!2spl!4v1674402785432!5m2!1spl!2spl"
           allowFullScreen={false}
-          loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
+          title="Lokalizacja na mapie google"
         />
         <ContactSection>
           <p>Jesteśmy z Warszawy, ale dojedziemy w każde miejsce w Polsce.</p>
