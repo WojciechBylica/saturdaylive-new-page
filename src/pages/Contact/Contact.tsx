2e64dc86fc11550bc9797b6888343beff9f0e1a2
Wojciech Bylica <wojtekbyl@gmail.com>
2023-01-29 22:02:01 +0100

    Contact page refactor and update

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,45 +1,18 @@
 import React from 'react';
 
-import { MainContentBox } from '../../components';
-import { email } from '../../media';
-import {
-  ContactSection,
-  ContactWrapper,
-  StyledMapIframe,
-  ContactMailIcon,
-  ContactMailLink,
-  ImportantParagraph
-} from './styled';
+import { ContactBox, MainContentBox } from '../../components';
+import { StyledMapIframe } from './styled';
 
-export const Contact = () => {
-  return (
-    <MainContentBox>
-      <h1>SaturdayLive - Kontakt</h1>
-      <ContactWrapper>
-        <StyledMapIframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2443.7556541686185!2d21.035567551120305!3d52.22965696535906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecd894bc9d9b5%3A0x2e8fdfac02717940!2ssaturdaylive%20-%20zesp%C3%B3%C5%82%20muzyczny%20na%20imprez%C4%99%20firmow%C4%85%20i%20wesele!5e0!3m2!1spl!2spl!4v1674402785432!5m2!1spl!2spl"
-          allowFullScreen={false}
-          referrerPolicy="no-referrer-when-downgrade"
-          title="Lokalizacja na mapie google"
-        />
-        <ContactSection>
-          <p>
-            Jesteśmy z Warszawy, ale dojedziemy w&nbsp;każde miejsce
-            w&nbsp;Polsce.
-          </p>
-          <ImportantParagraph>
-            Koncerty, Imprezy firmowe, Wesela - to nasz żywioł!
-          </ImportantParagraph>
-          <p>
-            W sprawie rezerwacji zespołu, oraz w&nbsp;razie jakichkolwiek pytań
-            prosimy o&nbsp;kontakt:
-          </p>
-          <ContactMailLink href={`mailto:${email}`}>
-            <ContactMailIcon />
-            {email}
-          </ContactMailLink>
-        </ContactSection>
-      </ContactWrapper>
-    </MainContentBox>
-  );
-};
+export const Contact = () => (
+  <MainContentBox>
+    <h1>SaturdayLive - Kontakt</h1>
+    <ContactBox />
+    <h2>Nasza lokalizacja</h2>
+    <StyledMapIframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2443.7556541686185!2d21.035567551120305!3d52.22965696535906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecd894bc9d9b5%3A0x2e8fdfac02717940!2ssaturdaylive%20-%20zesp%C3%B3%C5%82%20muzyczny%20na%20imprez%C4%99%20firmow%C4%85%20i%20wesele!5e0!3m2!1spl!2spl!4v1674402785432!5m2!1spl!2spl"
+      allowFullScreen={false}
+      referrerPolicy="no-referrer-when-downgrade"
+      title="Lokalizacja na mapie google"
+    />
+  </MainContentBox>
+);
--- src/pages/Contact/styled.ts
+++ src/pages/Contact/styled.ts
@@ -1,82 +1,11 @@
 import styled from 'styled-components';
-import { ReactComponent as MailBox } from '../../icons/mailbox.svg';
-
-export const ContactWrapper = styled.div`
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
-
-  @media (max-width: ${({ theme }) => theme.media.md}px) {
-    flex-direction: column-reverse;
-  }
-`;
 
 export const StyledMapIframe = styled.iframe`
-  width: 650px;
+  width: calc(100% - 32px);
   height: 300px;
   box-shadow: ${({ theme }) => theme.boxShadow};
-  margin: 16px;
+  margin: 16px 16px 32px 16px;
   border-radius: 10px;
   border: 0;
-  flex-basis: 45%;
-
-  @media (max-width: ${({ theme }) => theme.media.md}px) {
-    width: calc(100% - 32px);
-    flex-basis: initial;
-  }
-`;
-
-export const ContactSection = styled.section`
-  margin: 16px;
-  padding: 16px;
-  border: 1px solid #333;
-  border-radius: 10px;
-  box-shadow: ${({ theme }) => theme.boxShadow};
-  flex-basis: 45%;
-
-  @media (max-width: ${({ theme }) => theme.media.lg}px) {
-    width: calc(100% - 32px);
-  }
-`;
-
-export const ContactMailLink = styled.a`
-  color: ${({ theme }) => theme.color.primary};
-  font-size: 34px;
-  display: flex;
-  align-items: center;
-  width: fit-content;
-  margin-bottom: 16px;
-  text-decoration: none;
-  margin: 16px 0;
-  padding: 12px 16px;
-  border-radius: 10px;
-
-  @media (max-width: ${({ theme }) => theme.media.lg}px) {
-    font-size: 20px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.media.md}px) {
-    font-size: 34px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.media.sm}px) {
-    font-size: 20px;
-  }
-
-  &:hover {
-    color: ${({ theme }) => theme.color.secondary};
-    background: ${({ theme }) => theme.color.linkHoverBackground};
-    border-radius: 10px;
-  }
-`;
-
-export const ContactMailIcon = styled(MailBox)`
-  margin-right: 16px;
-  height: 34px;
-  width: 34px;
-`;
-
-export const ImportantParagraph = styled.p`
-  font-size: 20px;
-  font-weight: 700;
+  background: ${({ theme }) => theme.color.backgroundDark};
 `;
