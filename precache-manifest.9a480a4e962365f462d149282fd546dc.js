7761a07b1a469167cce1e36bf3b07b13812f2218
Wojciech Bylica <wojtekbyl@gmail.com>
2023-01-31 00:51:29 +0100

    Updates

---
+++ precache-manifest.9a480a4e962365f462d149282fd546dc.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b753e2cfdcb0e82d4e5",
+    "url": "/saturdaylive-new-page/static/js/main.3b753e2c.chunk.js"
+  },
+  {
+    "revision": "e7bb5da08af2ec7861fe",
+    "url": "/saturdaylive-new-page/static/css/1.eb15a52a.chunk.css"
+  },
+  {
+    "revision": "e7bb5da08af2ec7861fe",
+    "url": "/saturdaylive-new-page/static/js/1.e7bb5da0.chunk.js"
+  },
+  {
+    "revision": "3c8709207b98c905ba74",
+    "url": "/saturdaylive-new-page/static/js/2.3c870920.chunk.js"
+  },
+  {
+    "revision": "e33338afd957ba5dd84c",
+    "url": "/saturdaylive-new-page/static/js/3.e33338af.chunk.js"
+  },
+  {
+    "revision": "2023753bab88840d89ef",
+    "url": "/saturdaylive-new-page/static/js/4.2023753b.chunk.js"
+  },
+  {
+    "revision": "0542454ea2852123884a",
+    "url": "/saturdaylive-new-page/static/js/5.0542454e.chunk.js"
+  },
+  {
+    "revision": "51c0f9ef9bbdb3312d47",
+    "url": "/saturdaylive-new-page/static/js/runtime~main.51c0f9ef.js"
+  },
+  {
+    "revision": "9858fd954e570ec3666f98aacf99eab3",
+    "url": "/saturdaylive-new-page/static/media/facebook.9858fd95.svg"
+  },
+  {
+    "revision": "a19b822d08ded77fbe1e2d0c66227654",
+    "url": "/saturdaylive-new-page/static/media/instagram.a19b822d.svg"
+  },
+  {
+    "revision": "9e59e111951648d6171aef8e65ad1587",
+    "url": "/saturdaylive-new-page/static/media/youtube.9e59e111.svg"
+  },
+  {
+    "revision": "7cd579fcb8a03b3da4967c9d6ec36a61",
+    "url": "/saturdaylive-new-page/static/media/soundcloud.7cd579fc.svg"
+  },
+  {
+    "revision": "8f7c3bda9fd5bc58ed9805c23f81e8e7",
+    "url": "/saturdaylive-new-page/static/media/foto1.8f7c3bda.jpg"
+  },
+  {
+    "revision": "85a41ed5b83b8939a852f6214898aafb",
+    "url": "/saturdaylive-new-page/static/media/IMG_6574.85a41ed5.jpg"
+  },
+  {
+    "revision": "539129c693989443ab4d1e3dd5003809",
+    "url": "/saturdaylive-new-page/static/media/IMG_6578.539129c6.jpg"
+  },
+  {
+    "revision": "cfe2e97caecbff56fdb1547ff17c3cd3",
+    "url": "/saturdaylive-new-page/static/media/cert1.cfe2e97c.png"
+  },
+  {
+    "revision": "a42a23c1238e31818e42baaef7acc449",
+    "url": "/saturdaylive-new-page/static/media/cert2.a42a23c1.jpg"
+  },
+  {
+    "revision": "633953d3dfcadc7933e737effda1164f",
+    "url": "/saturdaylive-new-page/static/media/cert3.633953d3.jpg"
+  },
+  {
+    "revision": "142d01569a294eafe562a8af856371ed",
+    "url": "/saturdaylive-new-page/static/media/menu-down.142d0156.svg"
+  },
+  {
+    "revision": "2d9d693198160233762ee0392448f2d5",
+    "url": "/saturdaylive-new-page/static/media/mailbox.2d9d6931.svg"
+  },
+  {
+    "revision": "2f4351c7215b20e67d2305ff2b3d28ac",
+    "url": "/saturdaylive-new-page/static/media/lidl.2f4351c7.svg"
+  },
+  {
+    "revision": "b55660a7aad910c864ebf0eb8ed3f8a9",
+    "url": "/saturdaylive-new-page/static/media/casinos-poland.b55660a7.svg"
+  },
+  {
+    "revision": "320ca402eb9c968c61259c077253a22b",
+    "url": "/saturdaylive-new-page/static/media/ontraco.320ca402.svg"
+  },
+  {
+    "revision": "1b0d6908beaa9dd922383c1140375699",
+    "url": "/saturdaylive-new-page/static/media/ww.1b0d6908.svg"
+  },
+  {
+    "revision": "4e59aeb4cfe28a7bee565f72b253f6bd",
+    "url": "/saturdaylive-new-page/index.html"
+  }
+];
--- static/js/1.e7bb5da0.chunk.js
+++ static/js/1.e7bb5da0.chunk.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[1],{65:function(e,a,n){},84:function(e,a,n){"use strict";n.r(a);var o=n(1),t=n.n(o),r=n(67);n(65);a.default=function(e){var a=e.setHasLoaded,n=e.videoId;return t.a.createElement(r.a,{videoId:n,onReady:function(e){a(!0),e.target.playVideo()},className:"videoInner",iframeClassName:"videoInner",opts:{playerVars:{autoplay:1,rel:0}}})}}}]);
-//# sourceMappingURL=1.b9ee9316.chunk.js.map
+//# sourceMappingURL=1.e7bb5da0.chunk.js.map
